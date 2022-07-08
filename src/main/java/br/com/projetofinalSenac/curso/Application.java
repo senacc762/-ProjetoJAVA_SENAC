74967da217542d6fde656867b6f73214d31e9218
82well <72098782+82well@users.noreply.github.com>
2022-07-07 22:11:04 -0300

    Inicio
    Primeiro commit, projeto final

---
+++ src/main/java/br/com/projetofinalSenac/curso/Application.java
@@ -0,0 +1,20 @@
+package br.com.projetofinalSenac.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+/*@SpringBootApplication*/
+@SpringBootApplication
+
+public class Application {
+	
+    public static void main(String[] args) {
+    	
+        SpringApplication.run(Application.class, args);
+        
+    }
+}
