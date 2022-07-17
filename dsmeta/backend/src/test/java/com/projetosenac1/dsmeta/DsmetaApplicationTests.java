ce32e3119a18192addd143a6cb179184163b3b58
82well <nylook2@hotmail.com>
2022-07-17 14:35:40 -0300

    projeto finalizado
    Projeto final concluido API com envio de sms para celular cadastrado

---
+++ dsmeta/backend/src/test/java/com/projetosenac1/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosenac1.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
